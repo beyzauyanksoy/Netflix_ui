2b05a5f5dcef0f1d1948314e6f0de0be5ca618b0
Beyza <beyzauyanik57@hotmail.com>
2022-10-21 13:51:52 +0300

    providerItem oluşturuldu

--- lib/providerItem.dart
+++ lib/providerItem.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
 
 class ProvidertItem with ChangeNotifier {
-  Object? trending = {"image": "beyza"};
+  Map trending = {
+  "images": [
+    "assets/trending1.png",
+    "assets/trending2.png",
+    "assets/trending3.png",
+    "assets/trending4.png",
+  ]
+};
 
-  Object? originals = [
-    {
-      "image": [
-        "assets/originals1.png",
-        "assets/originals2.png",
-        "assets/originals3.png",
-        "assets/originals4.png",
-      ]
-    },
-  ];
-  Object? popular = [
-    {
-      "image": [
-        "assets/popular1.png",
-        "assets/popular2.png",
-        "assets/popular3.png",
-        "assets/popular4.png",
-      ]
-    },
-  ];
+  Map originals = {
+  "images": [
+    "assets/trending4.png",
+    "assets/originals2.png",
+    "assets/originals3.png",
+    "assets/originals4.png",
+  ]
+};
+ Map popular = {
+  "images": [
+    "assets/popular1.png",
+    "assets/popular2.png",
+    "assets/popular3.png",
+    "assets/popular4.png",
+  ]
+};
 }

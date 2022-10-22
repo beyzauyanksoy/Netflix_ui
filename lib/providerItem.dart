7872c8898df19f125a28a7a701905037f1107afc
Beyza Uyanıksoy <beyzauyanik57@hotmail.com>
2022-10-22 16:30:49 +0300

    navigateBottomBar dahil edildi

--- lib/providerItem.dart
+++ lib/providerItem.dart
@@ -1,31 +1,36 @@
 import 'package:flutter/material.dart';
 
 class ProvidertItem with ChangeNotifier {
+  int selectedIndex = 0;
 
-  
+  void navigateBottomBar(int index) {
+    selectedIndex = index;
+    print("index: $index");
+  }
+
   Map trending = {
-  "images": [
-    "assets/trending1.png",
-    "assets/trending2.png",
-    "assets/trending3.png",
-    "assets/trending4.png",
-  ]
-};
+    "images": [
+      "assets/trending1.png",
+      "assets/trending2.png",
+      "assets/trending3.png",
+      "assets/trending4.png",
+    ]
+  };
 
   Map originals = {
-  "images": [
-    "assets/trending4.png",
-    "assets/originals2.png",
-    "assets/originals3.png",
-    "assets/originals4.png",
-  ]
-};
- Map popular = {
-  "images": [
-    "assets/popular1.png",
-    "assets/popular2.png",
-    "assets/popular3.png",
-    "assets/popular4.png",
-  ]
-};
+    "images": [
+      "assets/originals1.png",
+      "assets/originals2.png",
+      "assets/originals3.png",
+      "assets/originals4.png",
+    ]
+  };
+  Map popular = {
+    "images": [
+      "assets/popular1.png",
+      "assets/popular2.png",
+      "assets/popular3.png",
+      "assets/popular4.png",
+    ]
+  };
 }

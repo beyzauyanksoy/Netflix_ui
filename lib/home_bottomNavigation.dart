e9cdf7275d42b3a3f7e8d16b39fa51ee369a3587
Beyza Uyanıksoy <beyzauyanik57@hotmail.com>
2022-10-22 16:33:16 +0300

    setState kaldırıldı, consumer eklendi

--- lib/home_bottomNavigation.dart
+++ lib/home_bottomNavigation.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:netflix_ui/providerItem.dart';
+import 'package:provider/provider.dart';
 
 class HomeBottomNavigation extends StatefulWidget {
   const HomeBottomNavigation({super.key});
@@ -10,31 +12,36 @@ class HomeBottomNavigation extends StatefulWidget {
 }
 
 class _HomeBottomNavigationState extends State<HomeBottomNavigation> {
-
-  int _selectedIndex=0;
-  void _navigateBottomBar(int index){
-      setState(() {
-        _selectedIndex=index;
-      });
-  }
+  // int _selectedIndex=0;
+  // void _navigateBottomBar(int index){
+  //     setState(() {
+  //       _selectedIndex=index;
+  //     });
+  // }
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-   
-      currentIndex: _selectedIndex,
-      onTap:_navigateBottomBar,
-    fixedColor: Color(0xff1B1919),
-      items: [
-        BottomNavigationBarItem(
-            icon: Image.asset('assets/home.png'), label: '',backgroundColor: Color(0xff1B1919)),
-        BottomNavigationBarItem(
-            icon: Image.asset('assets/download.png'), label: ''),
-        BottomNavigationBarItem(
-            icon: Image.asset('assets/search.png'), label: ''),
-        BottomNavigationBarItem(
-            icon: Image.asset('assets/bell.png'), label: ''),
-        BottomNavigationBarItem(icon: Image.asset('assets/menu.png'), label: '')
-      ],
+    return Consumer(
+      builder: (BuildContext context, ProvidertItem item, Widget? child) {
+        return BottomNavigationBar(
+          currentIndex: item.selectedIndex,
+          onTap: item.navigateBottomBar,
+          fixedColor: Color(0xff1B1919),
+          items: [
+            BottomNavigationBarItem(
+                icon: Image.asset('assets/home.png'),
+                label: '',
+                backgroundColor: Color(0xff1B1919)),
+            BottomNavigationBarItem(
+                icon: Image.asset('assets/download.png'), label: ''),
+            BottomNavigationBarItem(
+                icon: Image.asset('assets/search.png'), label: ''),
+            BottomNavigationBarItem(
+                icon: Image.asset('assets/bell.png'), label: ''),
+            BottomNavigationBarItem(
+                icon: Image.asset('assets/menu.png'), label: '')
+          ],
+        );
+      },
     );
   }
 }
